2ebb5b23d3ac4b5aa5e2c16952eddf443a55f704
wangyongdong <wydchn@gmail.com>
2018-06-25 16:17:39 +0800

    demo

--- example/demo.php
+++ example/demo.php
@@ -1,7 +1,7 @@
 <?php
 require('../init.php');
 
-$appid = 'wxb863ef9e5a9a844b';
-$secret = '9c8831d39c6fb23582370de7b6f15fac';
+$appid = '';
+$secret = '';
 $aShareInfo = WechatShare\WechatOpen::getShare($appid, $secret);
 print_r($aShareInfo);
